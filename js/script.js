e3e58a8faead53850d89016080be7cdb5db443ce
Michael Corcias <michael.corcias@gelt.com>
2024-11-30 23:43:29 +0100

    intentando otras formas

--- js/script.js
+++ js/script.js
@@ -16,25 +16,72 @@
 const divApp = document.getElementById("app")
 
 
-const getPokemon = async () => {
-    try {
-        const response = await fetch("https://pokeapi.co/api/v2/pokemon/")
-        if (!response.ok) {
-            throw new Error ("Ha surgido un error con la llamada API", response.status)            
+    const getPokemon = async (paginacion) => {
+
+        try {
+
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/?offset=${paginacion}&limit=10`)
+            if (!response.ok) {
+                throw new Error ("Ha surgido un error con la llamada API", response.status)            
+            }
+            const data = await response.json()
+            const array = data.results
+            return array
+            
+        } catch (error) {
+            console.log("Error CATCH al obtener los datos", error)
         }
-        const data = await response.json()
-        const array = data.results
-        return array
         
-    } catch (error) {
-        console.log("Error CATCH al obtener los datos", error)
     }
-    
-}
 
-console.log(getPokemon())
+    console.log(getPokemon(0))
 
-const template = (pokemons) => {
+    // DE FORMA COMPLEJA
+
+    // POR CADA VEZ QUE SE LLAMA A TEMPLATE -> HACE UN FOREACH EN DONDE SE LLAMA A GETPOKEMON2 PARA CADA ELEMENTO
+
+    const template = (pokemons) => {
+
+    pokemons.forEach((element) => getPokemon2(element))
+
+    }
+
+
+    // AQUÍ SE HACE FETCH DE CADA URL DE CADA POKEMON PARA BUSCAR SU INFORMACIÓN PARTICULAR
+
+    const getPokemon2 = async (pokemon) => {
+
+        try {
+
+            const responsePokemon = await fetch(`${pokemon.url}`)
+            if (!responsePokemon.ok) {
+                throw new Error ("Ha surgido un error con la llamada API 2", responsePokemon.status)            
+            }
+            const dataPokemon = await responsePokemon.json()
+            return dataPokemon
+            
+        } catch (error) {
+
+            console.log("Error CATCH 2 al obtener los datos", error)
+        }
+
+        console.log(dataPokemon);
+
+/*         divApp.innerHTML +=
+        `
+        <div>
+            <img src="${data.sprites.other.front_default}" alt="imagen pokemon ${data.name}">
+            <p>${data.name}</p>
+        </div>
+
+        ` */
+    }
+
+
+
+
+    // DE FORMA BUSCA VIDAS
+/* const template = (pokemons) => {
 
 pokemons.forEach(element => {
     const idPokemos = pokemons.indexOf(element) + 1
@@ -48,8 +95,8 @@ pokemons.forEach(element => {
     </div>
     
     `
-});
+}); 
   
-}
+}*/
 
-getPokemon().then((pokemon) => template(pokemon))
+getPokemon(0).then((listado) => template(listado))
